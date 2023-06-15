83b622e6ff48b2d4a97748abf9c9f15410d500a0
lucaos2001 <134880839+lucaos2001@users.noreply.github.com>
2023-06-14 21:42:52 -0300

    Add files via upload
    Exercício proposto:
    Suponha que você é o Dev responsável por codar a primeira parte de um sistema para atendimento de clientes por chat.   
    Visando automatizar parte da tarefa do atendente do chat, você precisa desenvolver um código em python que faça a inicialização de uma conversa.  
    - O sistema inicia enviando uma mensagem de boas vindas: “Que bom ter você aqui!” 
     - Depois precisa enviar a mensagem "Escrevendo..." 
     - Esperar 4 segundos   - Perguntar o nome do cliente (NomeCliente) 
     - Enviar uma mensagem: “Boa noite NomeCliente, em que posso ser útil?  (encerrar) *

---
+++ PrimeiraAulaCodefico.py
@@ -0,0 +1,10 @@
+import time
+
+print('Que bom ter você aqui!')
+print('Escrevendo...')
+time.sleep(4)
+print('Qual seu nome ?')
+a = input()
+print('Boa noite', a, 'em que posso ser útil?')
+time.sleep(4)
+print('Fim do programa B)')
